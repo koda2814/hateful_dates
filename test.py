497e5be54eedb83c4f33a06ecc91356d64310811
koda2814 <zeding.420@gmail.com>
2022-08-26 16:29:07 +1000

    first commit

---
+++ test.py
@@ -0,0 +1,49 @@
+class DataBase:
+	"""Класс для работы с БД"""
+
+	__instance = None
+
+	def __new__(cls, *args, **kwargs):
+		"""паттерн Singltone"""
+		if cls.__instance == None:
+			cls.__instance = super().__new__(cls)
+		return cls.__instance
+
+
+	def __init__(self, name, psw, port):
+		self.name = name
+		self.psw = psw
+		self.port = port
+
+	def print_info(self):
+		print(self.name, self.psw, self.port)
+
+db1 = DataBase('root', '1234', 8080)
+db2 = DataBase('toor', '5678', 8080)
+
+
+print(id(db1), id(db2))
+
+# db1.print_info()
+# db2.print_info()
+
+
+
+
+
+
+# class DataBase:
+# 	"""Класс для работы с БД"""
+# 	def get_data_from_db(self):
+# 		"""берет информацию из дб по указанному id"""
+# 		print("вызов из бд")
+
+# class CurrentUser:
+# 	def __init__(self, name='Vasya', age=19):
+# 		self.name = name
+# 		self.age = age
+# 		print('вызов __init__ внутри CurrentUser')
+# 		db.get_data_from_db()
+
+# db = DataBase()
+# user = CurrentUser()
