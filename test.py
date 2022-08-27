89ea6a759b7e6fd1cb6d8280a8fa517c91963da1
koda2814 <zeding.420@gmail.com>
2022-08-27 17:42:24 +1000

    database first steps

--- test.py
+++ test.py
@@ -1,8 +1,53 @@
+from multiprocessing import connection
+import vk_api
+import psycopg2
+from config import host, user, password, db_name
+
+
+
+try:
+	conn = psycopg2.connect(
+		host=host,
+		user = user,
+		password = password,
+		database = db_name,
+	)
+
+	conn.autocommit = True
+
+	with conn.cursor() as cursor:
+		cursor.execute(
+			"SELECT version();"
+		)
+		print(f'SERVER VERSION: {cursor.fetchone()}' )
+
+	with conn.cursor() as cursor:
+		cursor.execute(
+			"""CREATE TABLE users(
+			id INT,
+			name VARCHAR(15),
+			gender VARCHAR(6),
+			age INT,
+			city VARCHAR(30),
+			orientation VARCHAR(6),
+			description VARCHAR(500),
+			photo_link VARCHAR(50)
+			);"""
+		)
+	print('done')
+
+except psycopg2.Error as e:
+	print(f'PSQL ERROR WHILE CONNECT')
+	print(e)
+finally:
+	if conn:
+		conn.close()
+		print('PSQL CONNECTION CLOSED')
+
 class DataBase:
 	"""Класс для работы с БД"""
 
 	__instance = None
-
 	def __new__(cls, *args, **kwargs):
 		"""паттерн Singltone"""
 		if cls.__instance == None:
@@ -10,21 +55,26 @@ class DataBase:
 		return cls.__instance
 
 
-	def __init__(self, name, psw, port):
-		self.name = name
-		self.psw = psw
-		self.port = port
+	def __init__(self, host, user, password, db_name):
+		self.host = host
+		self.user = user
+		self.password = password
+		self.db_name = db_name
 
-	def print_info(self):
-		print(self.name, self.psw, self.port)
+	def to_connect(self):
+		self.connection = psycopg2.connect(
+		host=self.host,
+		user = self.user,
+		password =self.password,
+		database =self.db_name
+	)
+		self.connection.autocommit=True
 
-db1 = DataBase('root', '1234', 8080)
-db2 = DataBase('toor', '5678', 8080)
-print('hi')
-print('hello dude')
+	def close_connection(self):
+		if self.connection:
+			self.connection.close()
 
-
-print(id(db1), id(db2))
+db1 = DataBase('root', '1234', 8080, 'bot_users')
 
 # db1.print_info()
 # db2.print_info()
